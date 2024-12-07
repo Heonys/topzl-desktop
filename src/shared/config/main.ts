05cb25eac3f2be7cc8d8941d121482555e8d62c1
Jiheon Kim <siwmua0@naver.com>
2024-12-07 18:33:23 +0900

    :sparkles: App Config 셋업 함수 작성, Config 타입 및 유틸리티 타입 정의

--- src/shared/config/main.ts
+++ src/shared/config/main.ts
@@ -1,1 +1,48 @@
-export function setupConfig() {}
+import { app } from "electron";
+import fs from "fs-extra";
+import path from "node:path";
+import type { AppConfig } from "./type";
+import { getMainWindow } from "@/window/mainWindow";
+
+let _configPath: string;
+let cacheConfig: AppConfig;
+
+function getConfigPath() {
+  if (!_configPath) {
+    _configPath = path.join(app.getPath("appData"), "config.json");
+  }
+  return _configPath;
+}
+
+async function checkConfigPath() {
+  try {
+    await fs.readJson(getConfigPath());
+  } catch {
+    await fs.remove(getConfigPath());
+    await fs.writeJson(getConfigPath(), {});
+    cacheConfig = {};
+  }
+}
+
+export async function getAppConfig(): Promise<AppConfig> {
+  if (cacheConfig) return cacheConfig;
+  try {
+    const config = await fs.readJson(getConfigPath());
+    cacheConfig = config;
+  } catch {
+    const exists = await fs.pathExists(getConfigPath());
+    if (exists) {
+      fs.remove(getConfigPath());
+    }
+    await checkConfigPath();
+  }
+  return cacheConfig;
+}
+
+export async function setAppConfig() {
+  const mainWindow = getMainWindow();
+}
+
+export async function setupConfig() {
+  await checkConfigPath();
+}
--- src/shared/config/type.d.ts
+++ src/shared/config/type.d.ts
@@ -1,1 +1,49 @@
-// interface AppConfig {}
+type Config = {
+  common: {
+    language: string;
+  };
+  download: {
+    path: string;
+  };
+  lyric: {
+    fontColor: string;
+    fontSize: string;
+  };
+  shortCut: {
+    local: {
+      name: string;
+      age: number;
+    }[];
+    global: string[];
+  };
+};
+
+type KeyPaths<T extends object> =
+  T extends Record<string, any>
+    ? {
+        [K in keyof T]: K extends string
+          ? T[K] extends (infer R)[]
+            ? R extends object
+              ? K | `${K}.${number}` | `${K}.${number}.${KeyPaths<R>}`
+              : K | `${K}.${number}`
+            : T[K] extends Record<string, any>
+              ? K extends string
+                ? K | `${K}.${KeyPaths<T[K]>}`
+                : never
+              : K
+          : never;
+      }[keyof T]
+    : never;
+
+type KeyPathValue<
+  T extends Record<string, any>,
+  S extends string,
+> = S extends `${infer First}.${infer Rest}`
+  ? First extends keyof T
+    ? KeyPathValue<T[First], Rest>
+    : never
+  : T[S];
+
+export type AppConfig = Partial<Config>;
+export type AppConfigKeyPath = KeyPaths<Config>;
+export type AppconfigKeyPathValue<Path extends string> = KeyPathValue<Config, Path>;
