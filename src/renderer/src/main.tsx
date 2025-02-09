bf518037290d500fa0ef16752af6e4265a24500a
Jiheon Kim <siwmua0@naver.com>
2025-02-10 04:51:59 +0900

    :sparkles: React-router 버전 6.23.1 으로 다운그레이드 및 Topzl 버전 업데이트

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -5,7 +5,5 @@ import router from "@/routes";
 import "./global.css";
 
 bootstrap().then(() => {
-  createRoot(document.getElementById("app")!).render(
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />,
-  );
+  createRoot(document.getElementById("app")!).render(<RouterProvider router={router} />);
 });
--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -16,16 +16,6 @@ import {
 } from "@/pages";
 import App from "@/App";
 
-const options = {
-  future: {
-    v7_relativeSplatPath: true,
-    v7_fetcherPersist: true,
-    v7_normalizeFormMethod: true,
-    v7_partialHydration: true,
-    v7_skipActionErrorRevalidation: true,
-  },
-};
-
 const router = createHashRouter(
   [
     { path: "/pipmode", element: <PipmodePage /> },
@@ -52,7 +42,6 @@ const router = createHashRouter(
       ],
     },
   ],
-  options,
 );
 
 export default router;
